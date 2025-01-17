5db16b16866ecc8d55692b3fd0e7d89db9400824
RealFilllykung <filllykunggm@gmail.com>
2025-01-17 21:05:59 +0700

    feat: prepare service structure

---
+++ backend/record-service/infrastructures/interfaces/repositories/IPasswordRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace record_service.infrastructures.interfaces.repositories;
+
+public interface IPasswordRepository
+{
+    Task<T> CreatePassword<T>(T record) where T : class;
+    Task<T> ReadPassword<T>(T record) where T : class;
+    Task<T> UpdatePassword<T>(T record) where T : class;
+    Task<T> DeletePassword<T>(T record) where T : class;
+}
---
+++ backend/record-service/services/RecordService.cs
@@ -0,0 +1,8 @@
+﻿using record_service.infrastructures.interfaces;
+
+namespace record_service.services;
+
+public class RecordService : IRecordService
+{
+    
+}
