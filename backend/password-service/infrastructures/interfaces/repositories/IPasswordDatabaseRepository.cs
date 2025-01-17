efbf041bc4c63de9f15283ddf64cc87765ebeeb0
RealFilllykung <filllykunggm@gmail.com>
2025-01-17 23:09:45 +0700

    feat: password service initialization

---
+++ backend/password-service/infrastructures/interfaces/repositories/IPasswordDatabaseRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace password_service.infrastructures.interfaces.repositories;
+
+public interface IPasswordDatabaseRepository
+{
+    Task<string> GetPasswords();
+    Task<string> CreatePassword(string password);
+    Task<string> UpdatePassword(string password);
+    Task<string> DeletePassword(string password);
+}
---
+++ backend/password-service/infrastructures/interfaces/services/IPasswordService.cs
@@ -0,0 +1,9 @@
+﻿namespace password_service.infrastructures.interfaces.services;
+
+public interface IPasswordService
+{
+    Task<string> GetAllPasswords();
+    Task<string> CreateNewPassword(string password);
+    Task<string> UpdatePassword(string oldPassword, string newPassword);
+    Task<string> DeletePassword(string password);
+}
