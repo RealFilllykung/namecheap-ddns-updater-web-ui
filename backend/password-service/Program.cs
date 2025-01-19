3c9c9ed0faae0019213f62e3ea47378ecf411e1b
RealFilllykung <filllykunggm@gmail.com>
2025-01-19 22:26:49 +0700

    feat: generate symmetric file when open program

--- backend/password-service/Program.cs
+++ backend/password-service/Program.cs
@@ -1,6 +1,4 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using System.Security.Cryptography;
 using password_service.infrastructures.interfaces.services;
 using password_service.services;
 
@@ -10,6 +8,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 SetupDependencyInjection();
+SetupSymmetricKeyData();
 
 var app = builder.Build();
 
@@ -27,4 +26,31 @@ app.Run();
 void SetupDependencyInjection()
 {
     builder.Services.AddSingleton<IPasswordService, PasswordService>();
+}
+
+void SetupSymmetricKeyData()
+{
+    void CreateDirectory()
+    {
+        Directory.CreateDirectory("data");
+    }
+
+    void CreateSymmtricKey()
+    {
+        Aes aes = Aes.Create();
+        if (!File.Exists("data/symmetricKey.txt"))
+        {
+            using (FileStream fileStream = new FileStream("data/symmetricKey.txt", FileMode.Append, FileAccess.Write))
+            {
+                using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+                {
+                    binaryWriter.Write(aes.Key.Length);
+                    binaryWriter.Write(aes.Key);
+                }
+            }
+        }
+    }
+    
+    CreateDirectory();
+    CreateSymmtricKey();
 }
