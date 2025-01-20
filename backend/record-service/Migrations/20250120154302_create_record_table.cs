f3e1dba39f0eadc9ca228a4c3640a447304703fc
RealFilllykung <filllykunggm@gmail.com>
2025-01-20 23:26:50 +0700

    feat: add migration file

---
+++ backend/record-service/Migrations/20250120154302_create_record_table.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace record_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_record_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Records",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    domain = table.Column<string>(type: "text", nullable: false),
+                    encryptedPassword = table.Column<string>(type: "text", nullable: false),
+                    ip = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Records", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Records");
+        }
+    }
+}
