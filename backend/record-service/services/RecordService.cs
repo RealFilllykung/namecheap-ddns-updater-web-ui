1501ed349d2c87b5a793ed105e340a473874f862
RealFilllykung <filllykunggm@gmail.com>
2025-01-21 22:33:23 +0700

    feat: modify record model

--- backend/record-service/services/RecordService.cs
+++ backend/record-service/services/RecordService.cs
@@ -1,6 +1,8 @@
-﻿using record_service.infrastructures.interfaces;
+﻿using record_service.infrastructures.databases;
+using record_service.infrastructures.interfaces;
 using record_service.infrastructures.interfaces.repositories;
 using record_service.infrastructures.interfaces.services;
+using record_service.models;
 using record_service.models.requests;
 using record_service.models.responses;
 
@@ -11,12 +13,14 @@ public class RecordService : IRecordService
     private readonly IIPService _ipService;
     private readonly ILogger<RecordService> _logger;
     private readonly IPasswordRepository _passwordRepository;
+    private readonly DatabaseContext _databaseContext;
 
-    public RecordService(IIPService ipService, ILogger<RecordService> logger, IPasswordRepository passwordRepository)
+    public RecordService(IIPService ipService, ILogger<RecordService> logger, IPasswordRepository passwordRepository, DatabaseContext databaseContext)
     {
         _ipService = ipService;
         _logger = logger;
         _passwordRepository = passwordRepository;
+        _databaseContext = databaseContext;
     }
 
     public async Task<CreateRecordResponse> CreateRecord(CreateRecordRequest request)
@@ -27,7 +31,24 @@ public class RecordService : IRecordService
         string ip = await _ipService.GetCurrentPublicIP();
         _logger.LogInformation($"Obtained public IP of {ip}");
 
+        _logger.LogInformation($"Requesting password encryption");
         EncryptPasswordResponse? encryptPasswordResponse = await _passwordRepository.EncryptPassword(request.password);
+        _logger.LogInformation($"Done encrypting password: {encryptPasswordResponse?.encryptedPassword}");
+        
+        _logger.LogInformation($"Recording the record into database.");
+
+        RecordModel recordModel = new RecordModel
+        {
+            encryptedPassword = encryptPasswordResponse?.encryptedPassword,
+            domain = request.domain,
+            ip = ip
+        };
+        
+        _databaseContext.Records.Add(recordModel);
+        await _databaseContext.SaveChangesAsync();
+        
+        _logger.LogInformation($"Successfully created new record for {request.domain}");
+
         CreateRecordResponse response = new CreateRecordResponse
         {
             ip = ip,
