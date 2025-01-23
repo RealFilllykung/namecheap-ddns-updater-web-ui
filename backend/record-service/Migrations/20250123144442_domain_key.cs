c251d21be060c3055aefad3663500dfdec7f3fd6
RealFilllykung <filllykunggm@gmail.com>
2025-01-23 21:53:17 +0700

    feat: change primary key migration

---
+++ backend/record-service/Migrations/20250123144442_domain_key.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace record_service.Migrations
+{
+    /// <inheritdoc />
+    public partial class domain_key : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Records",
+                table: "Records");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "Records");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Records",
+                table: "Records",
+                column: "domain");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Records",
+                table: "Records");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "Records",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Records",
+                table: "Records",
+                column: "id");
+        }
+    }
+}
