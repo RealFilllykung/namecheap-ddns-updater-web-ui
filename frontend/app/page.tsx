5f60e167430939b04db09e57d6d43bdfde87420d
RealFilllykung <filllykunggm@gmail.com>
2025-01-31 23:29:55 +0700

    feat: add environment configuration

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,7 @@ import CreateNewRecordButton from "@/components/custom/create-new-record-button"
 import Header from "@/components/custom/header";
 import RecordTable from "@/components/custom/record-table";
 import { Toaster } from "@/components/ui/toaster";
+require('dotenv').config()
 
 export default function Home() {
   return (
