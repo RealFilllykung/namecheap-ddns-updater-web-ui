3d101e6e3989ecda066f2aa2564fefc98721e467
RealFilllykung <filllykunggm@gmail.com>
2025-01-31 23:14:38 +0700

    feat: create new record dialog

--- frontend/components/custom/create-new-record-button.tsx
+++ frontend/components/custom/create-new-record-button.tsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { Input } from "../ui/input";
 
 export default function CreateNewRecordButton()
 {
+    const [domain, setDomain] = useState('')
+    const [password, setPassword] = useState('')
     return(
         <div className="m-2">
-            <Button className="w-full" size="lg">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-                Create new record
-            </Button>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button className="w-full" size="lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        Create new record
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Create new record</DialogTitle>
+                        <DialogDescription>Please fill in your Namecheap domain and password for updating DDNS</DialogDescription>
+                    </DialogHeader>
+                    <Input placeholder="Domain name" onChange={(event) => setDomain(event.target.value)}></Input>
+                    <Input placeholder="Password" type="password" onChange={(event) => setPassword(event.target.value)}></Input>
+                    <DialogFooter>
+                        <Button>Save</Button>
+                        <Button variant="destructive">Cancel</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
         </div>
-
 )}
